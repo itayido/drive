0378a298e5ff003fe8ebea6a6a9145d45eb7d729
itayido <itayido@hilma.tech>
2025-11-23 17:59:20 +0200

    show works

--- server/routes/files.js
+++ server/routes/files.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var router = express.Router();
 var fs = require("fs").promises;
-const app = express();
 var path = require("path");
 
-app.use(express.json());
+var express = require("express");
+var router = express.Router();
+var fs = require("fs").promises;
+var path = require("path");
 
-/* GET files page */
-router.get("/:username", async function (req, res) {
+router.get("/:username", async (req, res) => {
   try {
     const basePath = path.join(
       __dirname,
@@ -17,25 +18,80 @@ router.get("/:username", async function (req, res) {
       req.params.username
     );
     const files = await fs.readdir(basePath);
+
     const resArr = [];
     for (const file of files) {
       const fullPath = path.join(basePath, file);
       const stats = await fs.stat(fullPath);
-      const obj = { name: file };
-      if (stats.isDirectory()) {
-        resArr.push(file + ": directory");
-      } else if (stats.isFile()) {
-        resArr.push(file + ": file");
-      }
+
+      if (stats.isDirectory()) resArr.push(file + ": directory");
+      if (stats.isFile()) resArr.push(file + ": file");
     }
+
     res.status(200).send(resArr);
   } catch (err) {
-    res.status(400).send("something went wrong", err);
+    console.error(err);
+    res.status(400).send("something went wrong");
   }
 });
 
-//delete  a file
+// show file content
+router.get("/:username/:fileName", async (req, res) => {
+  try {
+    const fullPath = path.join(
+      __dirname,
+      "..",
+      "public",
+      "users",
+      req.params.username,
+      req.params.fileName
+    );
+
+    let stats;
+    try {
+      stats = await fs.stat(fullPath);
+    } catch {
+      return res.status(404).send("File not found");
+    }
+
+    if (stats.isDirectory()) {
+      return res.status(400).send("Cannot read a directory");
+    }
+
+    const content = await fs.readFile(fullPath, "utf8");
+    res.status(200).send(content);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+// delete a file
 router.delete("/:username/:fileName", async (req, res) => {
+  try {
+    const fullPath = path.join(
+      __dirname,
+      "..",
+      "public",
+      "users",
+      req.params.username,
+      req.params.fileName
+    );
+
+    await fs.unlink(fullPath); // throws if not found
+    res.status(200).send(`Deleted: ${req.params.fileName}`);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      return res.status(404).send("File not found");
+    }
+    console.error(err);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+module.exports = router;
+
+router.get("/:username", async (req, res) => {
   try {
     const basePath = path.join(
       __dirname,
@@ -44,18 +100,38 @@ router.delete("/:username/:fileName", async (req, res) => {
       "users",
       req.params.username
     );
-
     const files = await fs.readdir(basePath);
 
-    const file = files.find((t) => t === req.params.fileName);
-    if (!file) {
-      return res.status(404).send("File not found");
+    const resArr = [];
+    for (const file of files) {
+      const fullPath = path.join(basePath, file);
+      const stats = await fs.stat(fullPath);
+
+      if (stats.isDirectory()) resArr.push(file + ": directory");
+      if (stats.isFile()) resArr.push(file + ": file");
     }
 
-    const fullPath = path.join(basePath, file);
-    await fs.unlink(fullPath);
+    res.status(200).send(resArr);
+  } catch (err) {
+    console.error(err);
+    res.status(400).send("something went wrong");
+  }
+});
 
-    res.status(200).send(`Deleted: ${file}`);
+// delete a file
+router.delete("/:username/:fileName", async (req, res) => {
+  try {
+    const fullPath = path.join(
+      __dirname,
+      "..",
+      "public",
+      "users",
+      req.params.username,
+      req.params.fileName
+    );
+
+    await fs.unlink(fullPath);
+    res.status(200).send(`Deleted: ${req.params.fileName}`);
   } catch (err) {
     console.error(err);
     res.status(500).send("Something went wrong");
