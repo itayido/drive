8414df08d00e44ed25df011965c06c98c6e3a322
itayido <itayido@hilma.tech>
2025-11-23 16:12:55 +0200

    signupold

--- drive-app/src/App.jsx
+++ drive-app/src/App.jsx
@@ -1,10 +1,46 @@
-import "./App.css";
+import "./css/App.css";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
+
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/register/register";
+import AddInfo from "./pages/register/AddInfo";
+
+import ProtectedRoute from "./ProtectedRoute";
+import Info from "./pages/home/components/Info";
+import Todo from "./pages/home/components/Todo";
+import Posts from "./pages/home/components/Posts";
+import PostDetails from "./pages/home/components/PostDetails";
+import Albums from "./pages/home/components/Albums";
+import AlbumDetails from "./pages/home/components/AlbumDetails";
 
 function App() {
   return (
     <>
       <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/register/additionalInfo" element={<AddInfo />} />
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/home" element={<Home />}>
+              <Route path="info" element={<Info />} />
+              <Route path="todolist" element={<Todo />} />
+
+              <Route path="posts" element={<Posts />} />
+              <Route path="posts/post/:postId" element={<PostDetails />} />
+
+              <Route path="albums" element={<Albums />} />
+              <Route path="albums/album/:id" element={<AlbumDetails />} />
+            </Route>
+          </Route>
+
+          <Route path="*" element={<h1>EROR 404</h1>} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ drive-app/src/pages/login.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router";
+
+function Login() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/users?username=${userName}`
+      );
+      const data = await response.json();
+
+      if (data.length === 0) {
+        setMessage("Username not found.");
+        return;
+      }
+
+      const user = data[0];
+
+      if (user.website === password) {
+        localStorage.setItem("ActiveUser", JSON.stringify(user));
+        navigate("/home");
+      } else {
+        setMessage("Incorrect password.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setMessage("Something went wrong");
+    }
+
+    setUserName("");
+    setPassword("");
+  }
+
+  return (
+    <>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="userName">Username: </label>
+        <input
+          id="userName"
+          type="text"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          required
+        />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <br />
+        <button type="submit">Login</button>
+        <br />
+        <Link to="../register">Haven't got an account yet? register here</Link>
+      </form>
+      {message && <p>{message}</p>}
+    </>
+  );
+}
+
+export default Login;
